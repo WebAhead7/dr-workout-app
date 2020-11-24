308b3e2a5f856bf77572d3669954e3dacc02b058
nizarhalloun <nshalloun@hotmail.com>
2020-11-24 23:31:10 +0200

    added buttons

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Button from '../components/Button';
+import './SignIn.css';
 const SignIn = () => {
   const [name, setName] = useState('');
 
   useEffect(() => {}, [name]);
 
   return (
-    <div>
-      <Button text="Sign up" />
-      <Button text="Sign in" />
-    </div>
+    <>
+      <div className="container">Dr Workout App</div>
+      <div className="container">
+        <Button text="Sign Up" />
+        <Button text="Sign In" />
+      </div>
+    </>
   );
 };
 
