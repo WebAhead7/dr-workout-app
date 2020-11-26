d2012c23a734ae027a477993a0bc06df09682907
Ammaryus <amaryousef@gmail.com>
2020-11-26 20:06:45 +0200

    counter fix...

--- src/pages/Workout.js
+++ src/pages/Workout.js
@@ -7,14 +7,17 @@ import CricyleCounter from "../components/CircyleCounter";
 function Workout() {
   const [workouts, setWorkouts] = React.useState(null);
   const [workoutIndex, setWorkoutIndex] = React.useState(0);
+  const [currentWorkout, setCurrentworkout] = React.useState(null);
+  const [workoutInterval, setWorkoutInterval] = React.useState(null);
   let { category } = useParams();
 
   React.useEffect(() => {
-    console.log("Workout mount");
-    getWorkouts(category).then((data) => setWorkouts(data));
-    return () => {
-      console.log("WorkOut Unmount");
-    };
+    getWorkouts(category).then((data) => {
+      setWorkouts(() => {
+        setCurrentworkout({ ...data[workoutIndex] });
+        return data;
+      });
+    });
   }, []);
 
   const handlePrev = (event) => {
@@ -23,18 +26,40 @@ function Workout() {
       nextIndex = nextIndex < 0 ? workouts.length - 1 : nextIndex;
       return nextIndex;
     });
+    setCurrentworkout({ ...workouts[workoutIndex] });
+    console.log(currentWorkout);
   };
 
   const handleNext = (event) => {
     setWorkoutIndex((prevIndex) => {
       return (prevIndex + 1) % workouts.length;
     });
+    setCurrentworkout({ ...workouts[workoutIndex] });
+    console.log(currentWorkout);
+  };
+
+  const handleStart = () => {
+    if (!workoutInterval) {
+      const currInterval = setInterval(() => {
+        // change the current
+        setCurrentworkout((prevWorkout) => {
+          const workout = { ...prevWorkout };
+          workout.time--;
+          return workout;
+        });
+      }, 1000);
+      setWorkoutInterval(currInterval);
+    }
+  };
+
+  const handlePause = () => {
+    clearInterval(workoutInterval);
+    setWorkoutInterval(null);
   };
 
   if (!workouts) {
     return <h1>Loading...</h1>;
   }
-  const currentWorkout = workouts[workoutIndex];
   return (
     <div className="workout-container">
       <div className="workout-logo">
@@ -45,9 +70,12 @@ function Workout() {
       </div>
       <h3>{currentWorkout.tip}</h3>
       <div className="timer-rounds-container">
-        <CricyleCounter title={"Time PR"} count={59} />
-        <CricyleCounter title={"Rounds"} count={10} />
-        <div className="controls-container">start pause</div>
+        <CricyleCounter title={"Time PR"} count={currentWorkout.time} />
+        <CricyleCounter title={"Rounds"} count={currentWorkout.repeate} />
+        <div className="controls-container">
+          <button onClick={handleStart}>Start</button>
+          <button onClick={handlePause}>Pause</button>
+        </div>
       </div>
       <button onClick={handlePrev}> Previous </button>
       <button onClick={handleNext}> Next </button>
